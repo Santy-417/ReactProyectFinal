c9eac7e3c0dd9e3584e0e75255d51fd0cc603bfd
Santy-417 <santichav417@gmail.com>
2024-05-20 11:42:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import PlayerTable from './components/PlayerTable.jsx';
+import NavBarHome from './components/NavBarHome.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <NavBarHome ShowLightButtons={true}/>
+      <PlayerTable ShowLightButtons={false}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBarHome.jsx
@@ -0,0 +1,52 @@
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavScrollExample() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+                <Navbar.Brand >
+                    <img src="/.components\Logo.png" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">HOME</Nav.Link>
+                        <Nav.Link href="#action2">NEWS</Nav.Link>
+                        <Nav.Link href="#action2">TEAM</Nav.Link>
+                        <NavDropdown title="SOCCER" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="PAGE" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="SHOP" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#action2">PUSHARSE</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavScrollExample;
