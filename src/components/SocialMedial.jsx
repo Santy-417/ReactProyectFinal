ef9265355f2bd4ebb3c8f8043b11405acca46822
Santy-417 <santichav417@gmail.com>
2024-05-21 23:42:24 -0500

    change player dairo

--- src/components/SocialMedial.jsx
+++ src/components/SocialMedial.jsx
@@ -3,8 +3,11 @@ import { Button } from "react-bootstrap"
 const SocialMedial = () => {
     return (
         <div className="footer">
-            <Button variant="outline-primary" style={{marginRight: "10px"}} size="sm">🔄</Button>
-            <Button variant="outline-primary" size="sm" href="" target="blank">🔄</Button> 
+            <Button variant="outline-primary" style={{ marginRight: "10px" }} size="sm">🔄</Button>
+            <Button variant="outline-primary" style={{ marginRight: "10px" }} size="sm">🔄</Button>
+            <Button variant="outline-primary" style={{ marginRight: "10px" }} size="sm">🔄</Button>
+            <Button variant="outline-primary" style={{ marginRight: "10px" }} size="sm">🔄</Button>
+            <Button variant="outline-primary" size="sm">🔄</Button>
         </div>
     )
 }
